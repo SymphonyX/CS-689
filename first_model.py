7463275f59129563ef13fb20c7be547379cf8028
John Lalor <lalor@cs.umass.edu>
2015-11-16 10:48:49 -0500

    bins added, svm and knn run

--- first_model.py
+++ first_model.py
@@ -3,6 +3,7 @@
 import os, sqlite3
 import numpy as np
 from sklearn import svm, linear_model
+from sklearn.neighbors import KNeighborsClassifier
 
 #code asssumes you have the ontime.sqlite3 database in pwd
 #db can be downloaded from
@@ -49,6 +50,45 @@ X = [np.array(z[0:4] + [carriers[z[4]]]+[z[6]]+[eval(z[7])] +
               [z[14] if type(z[14]) == int else 0]+
               [z[20] if type(z[20]) == int else 0]+
               [z[23] if type(z[20]) == int else 0]).astype(float) for z in data]
+
+y_depdelay_bins = []
+y_arrdelay_bins = []
+
+for r in data:
+    dd = r[11] if type(z[11]) == int else 0
+    ad = r[16] if type(z[16]) == int else 0
+
+    #bin departure
+    if dd <= 0: #on time or early
+        dd_bin = 0
+    elif dd <= 10: #up to 10 minutes delayed
+        dd_bin = 1
+    elif dd <= 30: #up to 30 minutes delayed
+        dd_bin = 2
+    elif dd <= 60: #up to 60 minutes delayed
+        dd_bin = 3
+    else: #60+ minutes delay
+        dd_bin = 4
+
+    #bin arrival
+    if ad <= 0: #on time or early
+        ad_bin = 0
+    elif ad <= 10: #up to 10 minutes delayed
+        ad_bin = 1
+    elif ad <= 30: #up to 30 minutes delayed
+        ad_bin = 2
+    elif ad <= 60: #up to 60 minutes delayed
+        ad_bin = 3
+    else: #60+ minutes delay
+        ad_bin = 4
+
+    y_arrdelay_bins.append(ad_bin)
+    y_depdelay_bins.append(dd_bin)
+    
+y_ad_bins = np.array(y_arrdelay_bins)
+y_dd_bins = np.array(y_depdelay_bins)
+
+
 y_cancelled = [np.array(z[17] if type(z[17]) == int else 0).astype(float) for z in data]
 y_depdelay = [np.array(z[11] if type(z[11]) == int else 0).astype(float) for z in data]
 y_arrdelay = [np.array(z[16] if type(z[16]) == int else 0).astype(float) for z in data]
@@ -59,11 +99,19 @@ X_test = X[train_size:]
 y_cancelled_train = y_cancelled[0:train_size]
 y_cancelled_test = y_cancelled[train_size:]
 
-y_depdelay_train = y_depdelay[0:train_size]
-y_depdelay_test = y_depdelay[train_size:]
+#these are continuous values below we use the discrete bins
+#y_depdelay_train = y_depdelay[0:train_size]
+#y_depdelay_test = y_depdelay[train_size:]
 
-y_arrdelay_train = y_arrdelay[0:train_size]
-y_arrdelay_test = y_arrdelay[train_size:]
+#y_arrdelay_train = y_arrdelay[0:train_size]
+#y_arrdelay_test = y_arrdelay[train_size:]
+
+y_depdelay_train = y_dd_bins[0:train_size]
+y_depdelay_test = y_dd_bins[train_size:]
+
+y_arrdelay_train = y_ad_bins[0:train_size]
+y_arrdelay_test = y_ad_bins[train_size:]
+
 
 
 #departure delay
@@ -71,25 +119,26 @@ print 'predict departure delay'
 clf = svm.SVC()
 clf.fit(X_train,y_depdelay_train)
 
-lreg = linear_model.LinearRegression()
-lreg.fit(X_train,y_depdelay_train)
+neigh = KNeighborsClassifier(n_neighbors=3)
+neigh.fit(X_train,y_depdelay_train)
 
 count_svm = 0
-count_reg = 0
+count_knn = 0
 
 for i in range(len(X_test)):
     y = clf.predict(X_test[i])
     if y == y_depdelay_test[i]:
         count_svm += 1
-    y = lreg.predict(X_test[i])
-    if abs(y - y_depdelay_test[i]) < 4:
-        count_reg += 1
+    y = neigh.predict(X_test[i])
+
+    if y == y_depdelay_test[i]:
+        count_knn += 1
         
 #print count_svm
 print '(SVM)test set correct: ' + str(count_svm * 1.0 / 200)
 
 #print count_reg
-print '(regression) test set correct: ' + str(count_reg * 1.0 / 200)
+print '(KNN) test set correct: ' + str(count_knn * 1.0 / 200)
 
 
 #arrival delay
@@ -97,27 +146,27 @@ print 'predict arrival delay'
 clf = svm.SVC()
 clf.fit(X_train,y_arrdelay_train)
 
-lreg = linear_model.LinearRegression()
-lreg.fit(X_train,y_arrdelay_train)
+neigh = KNeighborsClassifier(n_neighbors=3)
+neigh.fit(X_train,y_arrdelay_train)
 
 count_svm = 0
-count_reg = 0
+count_knn = 0
 
 for i in range(len(X_test)):
     y = clf.predict(X_test[i])
     if y == y_arrdelay_test[i]:
         count_svm += 1
         
-    y = lreg.predict(X_test[i])
-    if abs(y - y_arrdelay_test[i]) < 4:
-        count_reg += 1
+    y = neigh.predict(X_test[i])
+    if y == y_depdelay_test[i]:
+        count_knn += 1
 
        
 #print count_svm
 print '(SVM)test set correct: ' + str(count_svm * 1.0 / 200)
 
 #print count_reg
-print '(regression) test set correct: ' + str(count_reg * 1.0 / 200)        
+print '(KNN) test set correct: ' + str(count_knn * 1.0 / 200)        
 
 
 #cancelled
@@ -125,23 +174,24 @@ print 'predict cancellation'
 clf = svm.SVC()
 clf.fit(X_train,y_cancelled_train)
 
-lreg = linear_model.LinearRegression()
-lreg.fit(X_train,y_cancelled_train)
+neigh = KNeighborsClassifier(n_neighbors=3)
+neigh.fit(X_train,y_cancelled_train)
 
 count_svm = 0
-count_reg = 0
+count_knn = 0
 
 for i in range(len(X_test)):
     y = clf.predict(X_test[i])
     if y == y_cancelled_test[i]:
         count_svm += 1
-    y = lreg.predict(X_test[i])
-    if abs(y - y_cancelled_test[i]) < 4:
-        count_reg += 1
+
+    y = neigh.predict(X_test[i])
+    if y == y_cancelled_test[i]:
+        count_knn += 1
 
        
 #print count_svm
 print '(SVM)test set correct: ' + str(count_svm * 1.0 / 200)
 
 #print count_reg
-print '(regression) test set correct: ' + str(count_reg * 1.0 / 200)        
+print '(KNN) test set correct: ' + str(count_knn * 1.0 / 200)        
