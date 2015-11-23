9740d3f57572a83966b26eadb61ba5220a83b7fd
Dan Garant <dgarant@cs.umass.edu>
2015-11-23 14:19:09 -0500

    Adding ranking accuracy

--- one-day-horizon-models.R
+++ one-day-horizon-models.R
@@ -2,26 +2,69 @@ library(e1071)
 library(plyr)
 library(nnet)
 library(rpart)
+library(ranger)
+library(caret)
 
-dat <- read.csv("../data/features2-shuf.csv", nrows=5000)
+# eliminate any constant features -- typically only necessary when using a subset of the full data
+drop.const <- function(df) {
+  keep.cols <- aaply(colnames(df), 1, function(c) length(unique(df[, c])) > 1)
+  df <- df[, keep.cols]
+  df <- df[complete.cases(df), ]
+  return(df)
+}
+
+# eliminate any factor levels that appear only twice
+drop.rare.vals <- function(df) {
+  common.row <- t(aaply(colnames(df), 1, function(c) {
+    if(!is.factor(df[, c])) {
+      return(rep(TRUE, nrow(df)))
+    } else {
+      val.counts <- table(df[, c])
+      common.vals <- names(val.counts[val.counts > 2])
+      return(df[, c] %in% common.vals)
+    }
+  }))
+  remove.rows <- rowSums(common.row) != ncol(df)
+  return(df[!remove.rows, ])
+}
+
+# eliminate factors that have as many levels as rows
+drop.high.dim <- function(df) {
+  drop.factors <- aaply(colnames(df), 1, function(c) 
+    is.factor(df[, c]) && length(levels(df[, c])) == nrow(df))
+  df <- df[, !drop.factors]  
+  return(df)
+}
+
+split.train.test <- function(df) {
+  pairs <- unique(df$pair_id)
+  row.perm <- sample(length(pairs))
+  train.pairs <- pairs[row.perm[1:(0.7 * length(row.perm))]]
+  test.pairs <- pairs[row.perm[(0.7 * length(row.perm) + 1):length(row.perm)]]
+  return(list(train=df[df$pair_id %in% train.pairs, ],
+              test=df[df$pair_id %in% test.pairs, ]))
+}
+
+convert.probs.from.binary <- function(class.probs) {
+  mat <- cbind(1-class.probs, class.probs)
+  colnames(mat) <- c("NoDelay", "Delay")
+  return(mat)
+}
+
+dat <- read.csv("../data/flight-pairs.csv", nrows=5000)
+# for now, converting outcome to binary
+dat$DepDelay <- factor(ifelse(dat$DepDelay != "[-Inf,0]", "Delay", "NoDelay"), levels=c("NoDelay", "Delay"))
 
 dep.delay.features <- subset(dat, select=-c(ArrDelay, Cancelled, DepartureHour))
 dep.delay.features$DayOfWeek <- factor(dep.delay.features$DayOfWeek)
-
-# eliminate any constant features -- typically only necessary when using a subset of the full data
-keep.cols <- aaply(colnames(dep.delay.features), 1, function(c) length(unique(dep.delay.features[, c])) > 1)
-dep.delay.features <- dep.delay.features[, keep.cols]
-dep.delay.features <- dep.delay.features[complete.cases(dep.delay.features), ]
-
-# eliminate factors that have as many levels as rows
-drop.factors <- aaply(colnames(dep.delay.features), 1, function(c) 
-  is.factor(dep.delay.features[, c]) && length(levels(dep.delay.features[, c])) == nrow(dep.delay.features))
-dep.delay.features <- dep.delay.features[, !drop.factors]
+dep.delay.features <- drop.const(dep.delay.features)
+dep.delay.features <- drop.high.dim(dep.delay.features)
+dep.delay.features <- drop.rare.vals(dep.delay.features)
 
 # create 70/30 train/test split
-row.perm <- sample(nrow(dep.delay.features))
-dep.delay.train <- dep.delay.features[row.perm[1:(0.7 * length(row.perm))], ]
-dep.delay.test <- dep.delay.features[row.perm[(0.7 * length(row.perm) + 1):length(row.perm)], ]
+tt.split <- split.train.test(dep.delay.features)
+dep.delay.train <- tt.split$train
+dep.delay.test <- tt.split$test
 
 log.lik <- function(class.probs, actual.class) {
   return(sum(aaply(1:nrow(class.probs), 1, function(i) { 
@@ -32,8 +75,27 @@ log.lik <- function(class.probs, actual.class) {
 report.results <- function(method.name, class.probs) {
   estim.class <- aaply(class.probs, 1, function(r) names(r)[which.max(r)])
   num.correct <- sum(estim.class == dep.delay.test$DepDelay)
-  cat(method.name, ": ", num.correct, "/", nrow(dep.delay.test), " (", round(num.correct/nrow(dep.delay.test), 3), "%)\n", sep="")
+  cat(method.name, ": ", num.correct, "/", nrow(dep.delay.test), " (", round(num.correct * 100/nrow(dep.delay.test), 2), "%)\n", sep="")
   cat(method.name, " log-likelihood: ", log.lik(class.probs, dep.delay.test$DepDelay), "\n", sep="")
+  
+  dep.delay.test$zerodelayprob <- class.probs[, "NoDelay"]
+  # evaluate in terms of number of correctly ranked pairs
+  pair.results <- daply(dep.delay.test, .(pair_id), function(df) {
+    # assuming DepDelay is an ordinal factor
+    if(nrow(df) != 2) {
+      return(NA)
+    } else if(df[1, "DepDelay"] == df[2, "DepDelay"]) {
+      return(1)
+    } else if(as.numeric(df[1, "DepDelay"]) < as.numeric(df[2, "DepDelay"]) & 
+              df[1, "zerodelayprob"] > df[2, "zerodelayprob"]) {
+      return(1)
+    } else {
+      return(0)
+    }
+  })
+  num.correct <- sum(pair.results, na.rm=TRUE)
+  num.records <- length(pair.results[!is.na(pair.results)])
+  cat(method.name, " ranking accuracy: ", num.correct, "/", num.records, " (", round(num.correct * 100 / num.records, 2), "%)\n", sep="")
 }
 
 cat("~~~~~~~~~~~~~~~~~~~~~~~~~~~\n")
@@ -60,6 +122,7 @@ cat("~~~~~~~~~~~~~~~~~~~~~~~~~~~\n")
 # Multinomial logistic
 mmodel <- multinom(DepDelay ~ ., data=dep.delay.train, MaxNWts=5000, maxit=500, trace=FALSE)
 class.probs <- predict(mmodel, newdata=dep.delay.test, type="prob")
+class.probs <- convert.probs.from.binary(class.probs)
 report.results("Multinomial", class.probs)
 
 cat("~~~~~~~~~~~~~~~~~~~~~~~~~~~\n")
@@ -80,6 +143,11 @@ cat("~~~~~~~~~~~~~~~~~~~~~~~~~~~\n")
 gamma <- 0.02
 zero.weight <- 0.5
 cost <- 2.5
-svm.model <- svm(DepDelay ~ ., data=dep.delay.train, class.weights=c("[-Inf,0]" = zero.weight), probability=TRUE, gamma=gamma, cost=cost)
+svm.model <- svm(DepDelay ~ ., data=dep.delay.train, class.weights=c("NoDelay" = zero.weight), probability=TRUE, gamma=gamma, cost=cost)
 class.probs <- attr(predict(svm.model, newdata=dep.delay.test, probability=TRUE), "probabilities")
 report.results(paste0("SVM(gamma=", gamma, ", 0-weight=", zero.weight, ", C=", cost, ")"), class.probs)
+
+cat("~~~~~~~~~~~~~~~~~~~~~~~~~~~\n")
+rmodel <- ranger(DepDelay ~ ., data=dep.delay.train, probability=TRUE, write.forest=TRUE)
+class.probs <- predict(rmodel, data=dep.delay.test, probability=TRUE)$predictions
+report.results("Random forest", class.probs)
