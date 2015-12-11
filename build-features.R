88079c9fd10d21330b7c68c817c13f0bece60ee4
Dan Garant <dgarant@cs.umass.edu>
2015-12-11 12:38:04 -0500

    Performing some exploratory analyses

--- build-features.R
+++ build-features.R
@@ -8,6 +8,7 @@ small.dat <- subset(dat, select=c("Year", "Month", "DayofMonth", "DayOfWeek",
             "UniqueCarrier", "Origin", "Dest", "CRSDepTime", "CRSArrTime", "Distance", 
             "CRSElapsedTime", "DepDelay", "ArrDelay", "Cancelled"))
 
+small.dat$DepDelayMinutes <- small.dat$DepDelay
 small.dat$DepDelay <- cut(small.dat$DepDelay, breaks=c(-Inf, 0, 10, 30, 60, Inf), include.lowest=TRUE)
 small.dat$ArrDelay <- cut(small.dat$ArrDelay, breaks=c(-Inf, 0, 10, 30, 60, Inf), include.lowest=TRUE)
 small.dat$DepartureHour <- with(small.dat, ISOdatetime(Year, Month, DayofMonth, floor(CRSDepTime / 100), CRSDepTime %% 100, 0))
@@ -18,6 +19,8 @@ features <- merge(small.dat, airport_volume, by.x=c("Origin", "DepartureHour"), 
 
 airport_weather <- read.csv("../data/airport-weather.csv")
 features <- merge(features, airport_weather, by.x=c("Origin", "DepartureHour"), by.y=c("airport", "hour"), all.x=TRUE)
+# keep only records for which we have weather info
+features <- subset(features, !is.na(weatherSummary))
 
 write.csv(features, "../data/features2.csv", row.names=FALSE)
 
