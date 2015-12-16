d40d7979d45105ff5dc4a5db32ad35bb63423491
Dan Garant <dgarant@cs.umass.edu>
2015-12-16 09:29:57 -0500

    Classification performance for one-day horizon

--- create-figures.R
+++ create-figures.R
@@ -2,11 +2,20 @@ library(ggplot2)
 library(plyr)
 library(stringr)
 
-d <- read.csv("thirty-day-horizon-results-1.csv")
+d <- read.csv("one-day-horizon-results.csv")
 d$method <- str_replace(d$method, "Boosted classification.+", "Boosted classification")
 d$method <- str_replace(d$method, "SVM.+", "SVM")
 
 d.summ <- ddply(d, .(method, nrecs), summarize, ranking.acc.median=median(ranking.accuracy))
+
+g <- ggplot(subset(d, !(method %in% c("Uniform", "Random"))), aes(x=nrecs, y=class.accuracy)) + 
+  geom_boxplot(aes(x=factor(nrecs), fill=method)) + 
+  labs(x="Sample Size", y="Classification Accuracy") + 
+  theme_bw(base_size=16) + theme(legend.position="top") + guides(fill=guide_legend(title="Method", ncol=3))
+print(g)
+png("writeup/figures/class-performance-by-sample-size.png", width=800, height=400)
+print(g)
+dev.off()
 
 g <- ggplot(subset(d, !(method %in% c("Uniform", "Marginal", "Random"))), aes(x=nrecs, y=ranking.accuracy)) + 
     geom_boxplot(aes(x=factor(nrecs), fill=method)) + 
@@ -16,5 +25,6 @@ print(g)
 png("writeup/figures/performance-by-sample-size.png", width=800, height=400)
 print(g)
 dev.off()
+
 
 
