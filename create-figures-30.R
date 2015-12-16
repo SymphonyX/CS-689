08924d0986e3c7e7d8d935196b42b43779e9d469
John Lalor <lalor@cs.umass.edu>
2015-12-16 07:07:25 -0500

    30 day results

--- create-figures-30.R
+++ create-figures-30.R
@@ -2,7 +2,7 @@ library(ggplot2)
 library(plyr)
 library(stringr)
 
-d <- read.csv("thirty-day-horizon-results-1.csv")
+d <- read.csv("thirty-day-horizon-results-2.csv")
 d$method <- str_replace(d$method, "Boosted classification.+", "Boosted classification")
 d$method <- str_replace(d$method, "SVM.+", "SVM")
 
@@ -13,7 +13,7 @@ g <- ggplot(subset(d, !(method %in% c("Uniform", "Marginal", "Random"))), aes(x=
     labs(x="Sample Size", y="Ranking Accuracy") + geom_hline(yintercept=0.5, color="blue", size=2) + 
     theme_bw(base_size=16) + theme(legend.position="top") + guides(fill=guide_legend(title="Method", ncol=3))
 print(g)
-png("writeup/figures/performance-by-sample-size.png", width=800, height=400)
+png("writeup/figures/performance-by-sample-size-30.png", width=800, height=400)
 print(g)
 dev.off()
 
